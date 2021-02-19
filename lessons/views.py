87c503435bf5b1e10e36da2ae7daad26f3ea2c70
Will Mead <william-mead@outlook.com>
2021-02-19 03:32:54 +0000

    playing with context

--- lessons/views.py
+++ lessons/views.py
@@ -153,12 +153,9 @@ class ProfileView(LoginRequiredMixin, generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update({'monthly_earnings': get_total_earned(self.request.user)})
         context.update({'hours_taught': get_total_hours(self.request.user)})
         context.update({'total_earned': get_total_earned(self.request.user)})
-        # context.update({'monthly_earnings_json': get_all_monthly_earnings(self.request.user)})
-        context.update({'monthly_earnings': get_total_earned(self.request.user)})
-        context.update({'test': 'test'})
+        context.update({'monthly_earnings_json': get_all_monthly_earnings(self.request.user)})
 
         return context
 
