f7b707df748e28996c4bf70a6c3d38d654cb956c
Will Mead <william-mead@outlook.com>
2021-02-19 03:03:18 +0000

    added loggin

--- lessons/views.py
+++ lessons/views.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime
 import json
+import sys
 
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -23,6 +24,8 @@ def get_monthly_earnings(user):
 
 def get_all_monthly_earnings(user):
     monthly_earnings = {}
+    sys.stderr.write('Hello World')
+
     for month in range(1, 13):
         earnings = sum([lesson.duration_in_hours * lesson.student.rate_per_hour for lesson in user.profile.lesson_set.all() if lesson.date.month == month])
         monthly_earnings[month] = earnings
