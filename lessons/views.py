159f84a4f721ae0fe49bbb95b6e072e693a7920c
Will Mead <william-mead@outlook.com>
2021-02-19 02:55:42 +0000

    still fixing

--- lessons/views.py
+++ lessons/views.py
@@ -26,7 +26,6 @@ def get_all_monthly_earnings(user):
     for month in range(1, 13):
         earnings = sum([lesson.duration_in_hours * lesson.student.rate_per_hour for lesson in user.profile.lesson_set.all() if lesson.date.month == month])
         monthly_earnings[month] = earnings
-    print(monthly_earnings)
     return monthly_earnings
 
 
